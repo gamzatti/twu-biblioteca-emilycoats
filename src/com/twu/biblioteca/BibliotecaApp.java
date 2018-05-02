b72b5c7bfe0fb89328e3e02e2f79ec4f2e3ed72f
Emily Coats <ec@emilycoats.net>
2018-05-02 17:17:38 +1000

    Test for welcome message
    - Create new test file with class BibliotecaAppTest
    - Write one test for welcome message
    - Add static welcome method to BibliotecaApp

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -2,7 +2,10 @@ package com.twu.biblioteca;
 
 public class BibliotecaApp {
 
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
+    public static void main(String[] args){}
+
+    public static String welcome() {
+        String message = new String("Welcome to Biblioteca");
+        return message;
     }
 }
---
+++ test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -0,0 +1,13 @@
+package com.twu.biblioteca;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BibliotecaAppTest {
+
+    @Test
+    public void testWelcome() {
+        assertEquals("Welcome to Biblioteca", BibliotecaApp.welcome());
+    }
+}
